704453ec8759ce7efe9d5cc87677c98bce6f4366
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-09 16:55:49 +0300

    update

--- Assets/com.fair.ball.blocks/Scripts/Bullet.cs
+++ Assets/com.fair.ball.blocks/Scripts/Bullet.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private const float force = 100;
-    Rigidbody2D Rigidbody { get; set; }
+    private const float force = 30;
+    private Rigidbody2D Rigidbody { get; set; }
 
     private void Awake()
     {
+        Rigidbody = GetComponent<Rigidbody2D>();
+        Debug.Log(Rigidbody);
+    }
+
+    private void Start()
+    {
+        transform.position = FindObjectOfType<Player>().transform.position;
         Rigidbody.velocity = Player.Velocity * force;
     }
 }
