5336e68810dc243c5de90878f9ec0d8704e9e71a
Sladkiy Eduard <nikcxprojects@gmail.com>
2022-12-10 00:38:11 +0300

    update

---
+++ Assets/com.fair.ball.blocks/Scripts/Block.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Block : MonoBehaviour
+{
+    private int health;
+    private int Health
+    {
+        get => health;
+        set
+        {
+            health = value;
+            TextComponent.text = $"{health}";
+
+            Animation.Play();
+            OnCollisionEnter?.Invoke();
+
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private Animation Animation { get; set; }
+
+    private TextMeshPro TextComponent { get; set; }
+
+    public static Action OnCollisionEnter { get; set; }
+
+    private void Start()
+    {
+        Animation = GetComponent<Animation>();
+        TextComponent = GetComponentInChildren<TextMeshPro>();
+        Health = UnityEngine.Random.Range(10, 99);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Health--;
+    }
+}
